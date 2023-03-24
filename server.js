83f147bfd7351a2be53d07d8fb5837288fa09c81
Danielle Barker - Mac <caseybarker86@gmail.com>
2023-03-24 10:34:16 -0600

    created basic express/mongoose structure and basic server running

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.get("/test", (req, res) => {
+  res.send("test endpoint working");
+});
+app.listen(PORT, () => {
+  console.log("listening on port", PORT);
+});
